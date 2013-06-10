7fe55c47f0b2407d1332dca59eefcf82914b1613
Jordan Trevino <jordan.trevino@gmail.com>
2013-06-10 14:36:12 -0400

    ruby hw

---
+++ ruby/simple-arrays.rb
@@ -0,0 +1,19 @@
+# Construct an array with your favorite foods. It should have at least 5 elements.
+# Write a puts which returns your most favorite food out of the array.
+
+foods = ["falafel", "tacos", "seitan burritos", "kale", "beets"]
+puts foods[0]
+
+# Construct an array with the colors of the rainbow (ROYGBIV)
+# Slice the colors Red, Orange, and Yellow out of the array.
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+colors.slice(0,3)
+
+
+# Create an empty array.
+# Assign values to the the second and 6th position.
+
+empty_array = []
+empty_array[1]= "second"
+empty_array[5]="6th"
