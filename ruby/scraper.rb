e9130c31b89999d5198a843c63c36259a64ca92c
Jordan Trevino <jordan.trevino@gmail.com>
2013-06-13 09:15:52 -0400

    scraper

--- ruby/scraper.rb
+++ ruby/scraper.rb
@@ -1,17 +1,6 @@
-# New project
-
-# - Parse data from website (Ruby)
-#   - Parse all profiles
-#     - Parse one profile
-#       - Get their name, push to SQL table
+# Scraper
 
 
-
-
-# - Write sql commands to enter data (Sql)
-
-# - Documentation
-# - 
 
 require 'nokogiri'
 require 'sqlite3'
@@ -27,21 +16,6 @@ db.execute "CREATE TABLE IF NOT EXISTS Students(Id INTEGER PRIMARY KEY, Name TEX
 
 db.execute "INSERT INTO Students(Name) VALUES ('#{name}')"
 
-characteristics = {
-  :name = page.search("h4").first.text
-  :quote = page.search("h3").first # quote
-  # twitter = 
-  # linkedin =
-  :biography = $page.search("p").first.text
-  :education = $page.search(".services")[1].text
-  :work = $page.search(".services")[2].text
-
-  :treehouse = find_codercred("treehouse")
-  :codeschool = find_codercred("codeschool")
-  :coderwall = find_codercred("coderwall")]
-  :github = $page.search(".section-services a[href*='github']").attribute("href").value
-  :codeacademy = $page.search(".services a")[2].attribute("href").value
-}
 
 
 def find_codercred(codercred)
@@ -51,16 +25,36 @@ end
 
 $page.search(".services a")[1].attribute("href").value
 
+
+characteristics = {
+  :name => $page.search("h4").first.text,
+  :quote => $page.search("h3").first, # quote
+  :biography => $page.search("p").first.text,
+  :education => $page.search(".services")[1].text,
+  :work => $page.search(".services")[2].text,
+  :treehouse => find_codercred("treehouse"),
+  :codeschool => find_codercred("codeschool"),
+  :coderwall => find_codercred("coderwall"),
+  :github => $page.search(".section-services a[href*='github']").attribute("href").value,
+  :codeacademy => $page.search(".services a")[2].attribute("href").value
+}
+
 def determine_columns
-
-
 end
 
 
-# twitter
-# linkedin
-# coder cred
-# all social
-# biography
-# university
-# work
+# characteristics = {
+#   :name = page.search("h4").first.text
+#   :quote = page.search("h3").first # quote
+#   # twitter = 
+#   # linkedin =
+#   :biography = $page.search("p").first.text
+#   :education = $page.search(".services")[1].text
+#   :work = $page.search(".services")[2].text
+
+#   :treehouse = find_codercred("treehouse")
+#   :codeschool = find_codercred("codeschool")
+#   :coderwall = find_codercred("coderwall")]
+#   :github = $page.search(".section-services a[href*='github']").attribute("href").value
+#   :codeacademy = $page.search(".services a")[2].attribute("href").value
+# }
